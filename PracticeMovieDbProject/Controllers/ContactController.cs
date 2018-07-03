fc65f7f60fd690d43070d0737d8ff3f786624c93
Abhilash Hazarika <deapthcrisis@gmail.com>
2018-07-03 21:51:53 +0530

    Added Contact controller. Renamed display name to MovieDb shown on web pages.

---
+++ PracticeMovieDbProject/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PracticeMovieDbProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+    }
+}
